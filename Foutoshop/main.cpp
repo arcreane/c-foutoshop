3f3b5d77ea4d4183d38a0336ddb69ed853bc475e
Noémie ROCHARD <nrochard@student.42.fr>
2021-06-20 20:06:07 +0200

    filters ok

--- Foutoshop/main.cpp
+++ Foutoshop/main.cpp
@@ -1,20 +1,47 @@
 #include <opencv2/imgcodecs.hpp>
-   #include <opencv2/highgui.hpp>
-   #include <opencv2/imgproc.hpp>
-   #include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
 
-   using namespace cv;
-   using namespace std;
+using namespace cv;
+using namespace std;
 
+/////////////// Basic Functions //////////////////////
 
-   /////////////////  Images  //////////////////////
+int main() {
+    
+    //Récupération de l'image dans le dossier
+    string path = "Resources/test.png";
+    Mat img = imread(path);
+    
 
-   int main() {
+    //--- TRANSFORMATIONS DE L'IMAGE AVEC FILTRES --
+    
+    //Création des différentes images
+    Mat imgGray, imgBlur, imgCanny, imgDilate, imgErode;
+    
+    // Noir et blanc
+    cvtColor(img, imgGray, COLOR_BGR2GRAY);
+    
+    // Flou
+    GaussianBlur(imgGray, imgBlur, Size(7, 7), 5, 0);
+    
+    // Noir et blanc tracé
+    Canny(imgBlur, imgCanny, 25,75);
 
-       string path = "Resources/test.png";
-       Mat img = imread(path);
-       imshow("Image", img);
-       waitKey(0);
-       return 0;
+    Mat kernel = getStructuringElement(MORPH_RECT, Size(10, 10));
+    
+    // Noir et blanc tracé avec traits + épais
+    dilate(imgCanny, imgDilate, kernel);
+    
+    // Noir et blanc tracé avec traits - épais
+    erode(imgDilate, imgErode, kernel);
 
-   }
+    imshow("Image", img);
+    imshow("Image Gray", imgGray);
+    imshow("Image Blur", imgBlur);
+    imshow("Image Canny", imgCanny);
+    imshow("Image Dilation", imgDilate);
+    imshow("Image Erode", imgErode);
+    waitKey(0);
+}
